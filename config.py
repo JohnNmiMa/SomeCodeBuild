f81024f058573cd9d38fec9ed447fa508ee52263
John <johnmarksjr@gmail.com>
2014-12-05 13:14:21 -0700

    Facebook & Twitter update for PythonAnywhere

--- config.py
+++ config.py
@@ -12,13 +12,24 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 # WhooshAlchemy for db searches
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
 
-# OAuth stuff
+### OAuth stuff
 TOKEN_SECRET = SECRET_KEY
-FACEBOOK_APP_ID = '369725386526622'
-FACEBOOK_APP_SECRET = '805ad4815490679e06078b840e720e9b'
-TWITTER_CALLBACK_URL = 'http://somecode.herokuapp.com:5000'
-TWITTER_APP_KEY = 'fUAYjkBwFYf8Vp1Qsjy642krI'
-TWITTER_APP_SECRET = 'JI5O84Qrup8HBY74hIfD6tSLnGv4TGBIUj3UCXhi0Tp3MWxRWN'
+
+# for somecode.herokuapp.com
+#FACEBOOK_APP_ID = '369725386526622'
+#FACEBOOK_APP_SECRET = '805ad4815490679e06078b840e720e9b'
+# for somecode.pythonanywhere.com
+FACEBOOK_APP_ID = '1508116866113069'
+FACEBOOK_APP_SECRET = '35c9ceef9f1e43194ee9ef0e220680fb'
+
+# for somecode.herokuapp.com
+#TWITTER_CALLBACK_URL = 'http://somecode.herokuapp.com:5000'
+#TWITTER_APP_KEY = 'fUAYjkBwFYf8Vp1Qsjy642krI'
+#TWITTER_APP_SECRET = 'JI5O84Qrup8HBY74hIfD6tSLnGv4TGBIUj3UCXhi0Tp3MWxRWN'
+TWITTER_CALLBACK_URL = 'http://somecode.pythonanywhere.com:5000'
+TWITTER_APP_KEY = '4sR3apqOJKeLPdJxFWxiJc5sH'
+TWITTER_APP_SECRET = 'QdlDF3DE7HJuQOKYQgBTpztQCKLqIGYZD8pJjFmCcFCFty9GEr'
+
 GOOGLE_CLIENT_ID = '759451918691-6hb8d2up7algbjirfni465bcn743cjjb.apps.googleusercontent.com'
 GOOGLE_CLIENT_SECRET = 'sWrnh9N1YPx8wqSUggfxOxZn'
 
