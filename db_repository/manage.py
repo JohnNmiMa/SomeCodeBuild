e00915648c8a88890826453d1ba39a3d6e2b3d1d
John <johnmarksjr@gmail.com>
2014-12-23 23:43:51 -0700

    Add momentum scrolling for iOS

---
+++ db_repository/manage.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from migrate.versioning.shell import main
+
+if __name__ == '__main__':
+    main(six='<module 'six' from '/usr/local/Cellar/python/2.7.6/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/six.pyc'>')
